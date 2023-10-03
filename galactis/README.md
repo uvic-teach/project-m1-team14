ce5ff428377580a3e6b4df6a6a59d3b2e3851243
Brennan McMicking <brennanmcmicking@gmail.com>
2023-10-03 15:20:51 -0700

    add: galactis cdk

---
+++ galactis/README.md
@@ -0,0 +1,11 @@
+# Galactis
+
+Our identity service!
+
+## What does it do?
+
+Galactis handles logging in, registering an account, and validating a token. Users can create accounts and log in (to get a token) and other microservices can validate that a token they were given was for the correct account.
+
+## How do I develop this?
+
+Ask Brennan. I'm not going to bother writing much documentation because that will likely be a colossal waste of time.
---
+++ galactis/lib/galactis-stack.ts
@@ -0,0 +1,87 @@
+import * as cdk from "aws-cdk-lib";
+import { LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
+import { AttributeType, Table } from "aws-cdk-lib/aws-dynamodb";
+import { Function, Code, Runtime } from "aws-cdk-lib/aws-lambda";
+import { Construct } from "constructs";
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class GalactisStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const user_db = new Table(this, "UserTable", {
+      partitionKey: {
+        name: "username",
+        type: AttributeType.STRING,
+      },
+    });
+
+    const token_db = new Table(this, "TokenTable", {
+      partitionKey: {
+        name: "token",
+        type: AttributeType.STRING,
+      },
+    });
+
+    const register_handler = new Function(this, "RegisterHandler", {
+      runtime: Runtime.PYTHON_3_11,
+      code: Code.fromAsset("resources/register"),
+      handler: "register.handler",
+    });
+
+    const login_handler = new Function(this, "LoginHandler", {
+      runtime: Runtime.PYTHON_3_11,
+      code: Code.fromAsset("resources/login"),
+      handler: "login.handler",
+    });
+
+    const validate_handler = new Function(this, "ValidateHandler", {
+      runtime: Runtime.PYTHON_3_11,
+      code: Code.fromAsset("resources/validate"),
+      handler: "validate.handler",
+    });
+
+    user_db.grantReadData(login_handler);
+    user_db.grantFullAccess(register_handler);
+    user_db.grantReadData(validate_handler);
+
+    token_db.grantFullAccess(register_handler);
+    token_db.grantFullAccess(login_handler);
+    token_db.grantReadData(validate_handler);
+
+    const api = new RestApi(this, "GalactisRestApi", {
+      deployOptions: {
+        throttlingBurstLimit: 10,
+        throttlingRateLimit: 10,
+      },
+    });
+
+    const register_endpoint = api.root.addResource("register");
+    register_endpoint.addMethod(
+      "POST",
+      new LambdaIntegration(register_handler)
+    );
+
+    const login_endpoint = api.root.addResource("login");
+    login_endpoint.addMethod("POST", new LambdaIntegration(login_handler));
+
+    const validate_endpoint = api.root.addResource("validate");
+    validate_endpoint.addMethod(
+      "GET",
+      new LambdaIntegration(validate_handler),
+      {
+        apiKeyRequired: true,
+      }
+    );
+
+    const key = api.addApiKey("GalactisApiKey");
+    const plan = api.addUsagePlan("GalatisUsagePlan", {
+      name: "Prod",
+    });
+    plan.addApiKey(key);
+    plan.addApiStage({
+      api,
+      stage: api.deploymentStage,
+    });
+  }
+}
