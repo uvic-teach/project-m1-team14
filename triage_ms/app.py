42b15e35c792f5737e23036e877677ce94bc8376
Aman <73180294+palodaman@users.noreply.github.com>
2023-10-15 16:13:47 -0700

    api documentation changes

--- triage_ms/app.py
+++ triage_ms/app.py
@@ -1,12 +1,8 @@
 #openapi: 3.0.0
-#servers:
-  # Added by Swagger API
-#  - description: SwaggerHub Triage API
-#    url: https://virtserver.swaggerhub.com/palodaman/traigeMS/1.0.0
 #info:
-# version: "1.0.0"
-#  title: TraigeMS
-#description: The API for TriageMS
+#version: "1.0.0"
+#title: TraigeMS
+#description: The API to determine Triage results based on a form
 
 import os,json
 
@@ -23,17 +19,33 @@ def index():
 
 @app.route('/triage', methods=["POST"])
 def triageSymptoms():
-    """Processes the form answers in JSON format and returns traige result
-        ---
-        post:
-          description: sends the form answers in json format to the flask server to determine triage result
-
-        responses:
-          200:
-            description: return triage  in the form of a 3 key dictionary
-    """
+    
+    # Processes the form answers in JSON format and returns traige result
+    #     ---
+    #     post:
+    #       description: sends the form answers in json format to the flask server to determine triage result
+  
+    #     sample request body:
+    #     {
+    #     "pain_level":9,
+    #     "head_trauma":false,
+    #     "allergies":true,
+    #     "runny_nose":true,
+    #     "sore_throat":true,
+    #     "shortness_of_breath":true,
+    #     "inflammation":"mild",
+    #     "fever":true,
+    #     "cough":true,
+    #     "chest_pain":true,
+    #     "breathing_difficulty":true
+    # }
+    
+    #     responses:
+    #       200:
+    #         description: return triage in the form of a 3 key-value pair dictionary
+    
    symptomInput = request.json # JSON Body
-   responseData = {"result":"Futher Triage Needed",
+   responseData = {"result":"Further Triage Needed",
                    "cause":"na",
                    "medicine":"na"
                   } # Default Triage Result
@@ -45,22 +57,22 @@ def triageSymptoms():
       
       # Head Trauma
       if symptomInput["head_trauma"]:
-          responseData["result"] = "Futher Triage Needed"
+          responseData["result"] = "Further Triage Needed"
 
       # inflammation
       elif symptomInput["inflammation"] in ["moderate", "severe"]:
-          responseData["result"] = "Futher Triage Needed"
+          responseData["result"] = "Further Triage Needed"
           responseData["cause"] = "Possible infection"
 
       # Chest Pain
       elif symptomInput["chest_pain"]:
-        responseData["result"] = "Futher Triage Needed"
+        responseData["result"] = "Further Triage Needed"
         responseData["cause"] = "Chest Pain"
 
       # Breathing
       elif symptomInput["breathing_difficulty"]:
         responseData["cause"] = "Difficulty Breathing"
-        responseData["result"] = "Futher Triage Needed"
+        responseData["result"] = "Further Triage Needed"
 
       # Allergy Symptoms
       elif symptomInput["allergies"]:
@@ -87,7 +99,7 @@ def triageSymptoms():
                    responseData["medicine"] = "Cold Medicine"
                 else:
                    responseData["cause"] = "Possible Infection"
-                   responseData["result"] = "Futher Triage Needed"
+                   responseData["result"] = "Further Triage Needed"
             
 
    return responseData
