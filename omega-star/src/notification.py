ead5c345dc74cd531dd656f7bf9759c02bb00e33
Kyle Stang <33678573+kylestang@users.noreply.github.com>
2023-11-07 13:44:18 -0800

    Add email

---
+++ omega-star/src/notification.py
@@ -0,0 +1,19 @@
+from os import environ
+import smtplib
+from email.message import EmailMessage
+
+
+def send_email(sender: str, to: str, subject: str, contents: str):
+    email_user = environ.get("EMAIL_USER")
+    email_pass = environ.get("EMAIL_PASS")
+
+    msg = EmailMessage()
+    msg['Subject'] = subject
+    msg['From'] = sender
+    msg['To'] = to
+    msg.set_content(contents)
+
+    server = smtplib.SMTP('localhost', 1025)
+    server.login(email_user, email_pass)
+    server.send_message(msg)
+    server.quit()
