ba0c138b280479d58a2c821cd6c8bc5783817e9b
Brennan McMicking <brennanmcmicking@gmail.com>
2023-11-21 19:21:44 -0800

    change: unique bucket and cloudfront distribution for frontend deployment

--- raccoon/cdk/lib/frontend-stack.ts
+++ raccoon/cdk/lib/frontend-stack.ts
@@ -1,24 +1,92 @@
 import * as cdk from "aws-cdk-lib";
-import { Bucket } from "aws-cdk-lib/aws-s3";
+import {
+  Certificate,
+  CertificateValidation,
+} from "aws-cdk-lib/aws-certificatemanager";
+import {
+  AllowedMethods,
+  Distribution,
+  OriginAccessIdentity,
+  SecurityPolicyProtocol,
+  ViewerProtocolPolicy,
+} from "aws-cdk-lib/aws-cloudfront";
+import { S3Origin } from "aws-cdk-lib/aws-cloudfront-origins";
+import { CanonicalUserPrincipal, PolicyStatement } from "aws-cdk-lib/aws-iam";
+import { ARecord, HostedZone, RecordTarget } from "aws-cdk-lib/aws-route53";
+import { CloudFrontTarget } from "aws-cdk-lib/aws-route53-targets";
+import { BlockPublicAccess, Bucket } from "aws-cdk-lib/aws-s3";
 import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
 import { Construct } from "constructs";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class SiteDeployStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const bucket = Bucket.fromBucketArn(
+    const app_name = "Seng350";
+
+    const zone = HostedZone.fromLookup(this, `${app_name}Seng350HostedZone`, {
+      domainName: "brennanmcmicking.net",
+    });
+    const siteDomain = "seng350.brennanmcmicking.net";
+
+    const cloudfrontOAI = new OriginAccessIdentity(
       this,
-      "DestinationBucket",
-      "arn:aws:s3:::www.brennanmcmicking.net"
+      `${app_name}cloudfront-OAI`,
+      {
+        comment: `OAI for ${id}`,
+      }
     );
 
-    new BucketDeployment(this, "BucketDeployment", {
+    const bucket = new Bucket(this, `${app_name}SiteBucket`, {
+      bucketName: siteDomain,
+      publicReadAccess: false,
+      blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    bucket.addToResourcePolicy(
+      new PolicyStatement({
+        actions: ["s3:GetObject"],
+        resources: [bucket.arnForObjects("*")],
+        principals: [
+          new CanonicalUserPrincipal(
+            cloudfrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId
+          ),
+        ],
+      })
+    );
+
+    const certificate = new Certificate(this, `${app_name}SiteCertificate`, {
+      domainName: siteDomain,
+      validation: CertificateValidation.fromDns(zone),
+    });
+
+    const distribution = new Distribution(this, `${app_name}SiteDistribution`, {
+      certificate,
+      defaultRootObject: "index.html",
+      domainNames: [siteDomain],
+      minimumProtocolVersion: SecurityPolicyProtocol.TLS_V1_2_2021,
+      defaultBehavior: {
+        origin: new S3Origin(bucket, {
+          originAccessIdentity: cloudfrontOAI,
+        }),
+        compress: true,
+        allowedMethods: AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
+        viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+    });
+
+    new ARecord(this, `${app_name}SiteAliasRecord`, {
+      recordName: siteDomain,
+      target: RecordTarget.fromAlias(new CloudFrontTarget(distribution)),
+      zone,
+    });
+
+    new BucketDeployment(this, `${app_name}BucketDeployment`, {
       sources: [Source.asset("../raccoon/build")],
       destinationBucket: bucket,
-      destinationKeyPrefix: "seng350/",
-      distributionPaths: ["/seng350/*"],
+      distribution,
+      distributionPaths: ["/*"],
     });
   }
 }
